eb448e8f5de01673247c934653870c12acf1ac84
Jared <jaredbolduc@yahoo.com>
2019-05-15 19:18:42 -0600

    saving

---
+++ src/Components/Forms/Forms.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Forms.css';
+class Dashboard extends Component {
+    render(){
+        return (
+            <div className="Forms">
+                Forms
+            </div>
+        );
+    }
+}
+
+export default Forms;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Product.css';
+class Product extends Component {
+    render(){
+        return (
+            <div className="Product">
+                Product
+            </div>
+        );
+    }
+}
+
+export default Product;
